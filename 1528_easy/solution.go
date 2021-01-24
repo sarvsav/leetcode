4b74a8ad972050a5ce5eab0e6d3360f6de1f9be2
Sarvsav Sharma <sarvsav@gmail.com>
2021-01-24 18:49:16 +0530

    Problem: Shuffle String

---
+++ 1528_easy/solution.go
@@ -0,0 +1,26 @@
+// Problem: 
+
+// In beginning, I tried like this
+
+func restoreString(s string, indices []int) string {
+    stringSlice := make([]string, len(s))
+    for i:=0; i< len(indices); i++ {
+        stringSlice[indices[i]] = string(s[i])
+    }
+    return strings.Join(stringSlice[:], "")
+}
+
+// then realized, that I can a byte array, and easily convert to string using
+// string
+
+// For example:
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	byteSlice := []byte{'c','o','d','i','n','g','t','h','e','r','i','g','h','t','w','a','y'}
+	fmt.Println(string(byteSlice)) 
+}
