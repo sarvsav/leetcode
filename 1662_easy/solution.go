deb4fa1923837bbc67d6ef343ef33bfeb9aae895
Sarvsav Sharma <sarvsav@gmail.com>
2021-02-14 13:12:38 +0530

    Problem: Check If Two String Arrays are Equivalent

---
+++ 1662_easy/solution.go
@@ -0,0 +1,11 @@
+// Problem: https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
+import "strings"
+
+func arrayStringsAreEqual(word1 []string, word2 []string) bool {
+	string1 := strings.Join(word1, "")
+	string2 := strings.Join(word2, "")
+	if string1 == string2 {
+		return true
+	}
+	return false
+}
